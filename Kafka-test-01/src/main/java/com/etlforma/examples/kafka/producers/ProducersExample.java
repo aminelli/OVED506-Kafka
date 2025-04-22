8e6bb6b36a2ed744824b47b778fd9674e96e4661
aminelli <antonio.minelli@etlforma.com>
2025-04-22 17:36:40 +0200

    upd

--- Kafka-test-01/src/main/java/com/etlforma/examples/kafka/producers/ProducersExample.java
+++ Kafka-test-01/src/main/java/com/etlforma/examples/kafka/producers/ProducersExample.java
@@ -18,7 +18,8 @@ public class ProducersExample {
 
         // producer01.sendMessagesFireAndForget("T_FireAndForget", 1);
         // producer01.sendMessagesAck1Sync("T_Ack1_sync", 1);
-        producer01.sendMessagesAck1Async("T_Ack1_async", 10000);
+        // producer01.sendMessagesAck1Async("T_Ack1_async", 10000);
+        producer01.sendMessagesAckAllSync("T_Ack_all_sync", 10000);
 
 
     }
@@ -243,11 +244,74 @@ public class ProducersExample {
         producer.close();
         System.out.println("Done \r\n\r\n");
 
-
-
-
     }
 
 
+    // ACK = all con sync
+    public void sendMessagesAckAllSync(String topicName, long totalMessage) {
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        Properties props = new Properties();
+
+        // Connessione al cluster
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+
+        // Client ID
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "ProducerFAF");
+
+        // Compression Type
+        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "gzip");
+
+        // TIPO ACK = 0
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+
+        // Namespaces classi per serializzazione
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,   "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        ProducerRecord<String, String> record = null;
+
+        String startTime = formatter.format(new Date());
+
+        for (int count = 0; count < totalMessage; count++) {
+
+            record = new ProducerRecord<String, String>(topicName, "Messaggio nr " + count);
+            String headKey = "K_" + startTime + "_" + count;
+
+            try {
+                record.headers().add("MSG_KEY", headKey.getBytes());
+                Future<RecordMetadata> future = producer.send(record);
+
+                RecordMetadata metadata = future.get();
+
+
+                System.out.println("Sent message -> " + headKey);
+
+                System.out.println("Partition -> " + metadata.partition());
+                System.out.println("Offset -> " + metadata.offset());
+                System.out.println("======================================");
+
+
+            } catch (Exception ex) {
+                System.out.println("Error Msg -> " + headKey);
+
+            }
+
+        }
+
+        String endTime = formatter.format(new Date());
+
+        System.out.println("Start: -> " + startTime);
+        System.out.println("End:   -> " + endTime);
+
+        producer.flush();
+        producer.close();
+        System.out.println("Done \r\n\r\n");
+
+    }
 
 }
